4bc4c2fb86cf73ffa73365cbac4f4aa433d8e534
Sheri Wong <syw787@gmail.com>
2018-10-13 18:24:58 -0400

    first publish

---
+++ client.py
@@ -0,0 +1,40 @@
+import socket
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
+
+
+client = socket.socket()
+ip = socket.gethostbyname(socket.gethostname())
+port = 9500
+address = (ip,port)
+
+client.connect(address)
+
+
+
+def communicate(data):
+
+    file_out = open("encrypted_data.bin", "wb")
+
+    recipient_key = RSA.import_key(open("receiver.pem").read())
+    session_key = get_random_bytes(16)  
+
+# Encrypt the session key with the public RSA key
+    cipher_rsa = PKCS1_OAEP.new(recipient_key)
+    enc_session_key = cipher_rsa.encrypt(session_key)
+
+# Encrypt the data with the AES session key
+    cipher_aes = AES.new(session_key, AES.MODE_EAX)
+    ciphertext, tag = cipher_aes.encrypt_and_digest(data)
+    [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
+    #client.send(data)
+    #print (client.recv(1024))
+    return
+
+
+communicate("Hello this is a message.".encode("utf-8"))
+
+
+
+
---
+++ server.py
@@ -0,0 +1,46 @@
+import socket
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES, PKCS1_OAEP
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+ip = socket.gethostbyname(socket.gethostname())
+port = 9500 
+address = (ip,port)
+
+server.bind(address)
+server.listen(1)
+print ("start listening",ip,port)
+
+
+client,addr = server.accept()
+print ("Got a connection from", addr[0], addr[1])
+
+
+
+while True: 
+    data = client.recv(1024)
+    file_in = open("encrypted_data.bin", "rb")
+
+    private_key = RSA.import_key(open("private.pem").read())
+
+    enc_session_key, nonce, tag, ciphertext = \
+        [ file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
+
+# Decrypt the session key with the private RSA key
+    cipher_rsa = PKCS1_OAEP.new(private_key)
+    session_key = cipher_rsa.decrypt(enc_session_key)
+
+# Decrypt the data with the AES session key
+    cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+    data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+    print(data.decode("utf-8"))
+    break
+
+    
+
+
+
+
+        
+  
+     
